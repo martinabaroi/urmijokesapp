670715e73df83622d37f03472dd05a4841ffb57c
Martina Baroi <m_baroi@yahoo.com>
2022-08-17 15:35:21 -0400

    initial project setup

---
+++ src/main/java/urmi/springsamples/urmijokesapp/UrmijokesappApplication.java
@@ -0,0 +1,13 @@
+package urmi.springsamples.urmijokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrmijokesappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrmijokesappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/urmi/springsamples/urmijokesapp/UrmijokesappApplicationTests.java
@@ -0,0 +1,13 @@
+package urmi.springsamples.urmijokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrmijokesappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
